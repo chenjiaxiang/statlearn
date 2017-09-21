ebc64ad9e840a6ea08f5ff0ee102a5c2f473deab
chenjiaxiang <1289609489@qq.com>
2017-09-21 21:35:47 +0800

    Update optimizor.py

--- optimizor.py
+++ optimizor.py
@@ -15,7 +15,7 @@ cons = ({'type':'eq',
 		 'jac':lambda x:np.array([1.0,1.0])},
 		{'type':'ineq',
 		 'fun':lambda x:np.array([-(x[0]**2)+x[1]]),
-		 'jac':lambda x:np.array([-2.0*x[0],-1.0])})
+		 'jac':lambda x:np.array([-2.0*x[0],1.0])})
 
-res = minimize(func,[-1.0,2.0],args=(1.0,),jac=func_deriv,constraints=cons,method='SLSQP',options={'disp':True})
+res = minimize(func,[-1.0,1.0],args=(1.0,),jac=func_deriv,constraints=cons,method='SLSQP',options={'disp':True})
 print(res.x)
